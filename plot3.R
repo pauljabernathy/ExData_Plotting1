c0d222939b1371f5646d49db218834a670cbc7eb
pauljabernathy <pauljabernathy@gmail.com>
2014-05-12 07:20:12 -0400

    added R and png files again

--- plot3.R
+++ plot3.R
@@ -5,5 +5,9 @@ e <- sqldf("select * from e where Date='1/2/2007' or Date='2/2/2007'")
 datetime <- dmy_hms(paste(e$Date, e$Time))
 e <- cbind(e, datetime)
 png(file="plot3.png");
-plot(e$datetime, e$Voltage, type="l", xlab="datetime", ylab="Voltage")
+with(e, plot(datetime, Sub_metering_1, type="n"))
+with(e, points(datetime, Sub_metering_1, type="l", col="black"))
+with(e, points(datetime, Sub_metering_2, type="l", col="red"))
+with(e, points(datetime, Sub_metering_3, type="l", col="blue"))
+legend("topright", lty=1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off();
--- plot4.R
+++ plot4.R
@@ -6,8 +6,20 @@ datetime <- dmy_hms(paste(e$Date, e$Time))
 e <- cbind(e, datetime)
 png(file="plot4.png");
 
+par(mfrow=c(2, 2))
+
+#top left
 plot(e$datetime, e$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l");
+
+#top right
 plot(e$datetime, e$Voltage, type="l", xlab="datetime", ylab="Voltage")
-#other goes here
+
+#bottom left
+with(e, plot(datetime, Sub_metering_1, type="n"))
+with(e, points(datetime, Sub_metering_1, type="l", col="black"))
+with(e, points(datetime, Sub_metering_2, type="l", col="red"))
+with(e, points(datetime, Sub_metering_3, type="l", col="blue"))
+
+#bottom right
 plot(e$datetime, e$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off();
